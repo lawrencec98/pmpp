ba09e01854eb1fa47c5afe236d854c1a5d1edacb
lcatapang3 <lcatapang3@gatech.edu>
2025-12-20 19:37:34 +0000

    Helper function: Matrix initializer using CPU and GPU

--- basicmatmul.cu
+++ basicmatmul.cu
@@ -29,11 +29,32 @@ __global__ void MatrixMultiply(float* matrixA, float* matrixB, float* matrixResu
 }
 
 
+void InitializeMatrixCPU(float* matrix, int width)
+{
+    for (int i = 0; i < width*width; ++i)
+    {
+        matrix[i] = i;
+    }
+}
+
+
+__global__ void InitializeMatrixGPU(float* matrix, int width)
+{
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < width && col < width)
+    {
+        matrix[row * width + col] = row*width + col;
+    }
+}
+
+
 void DisplayMatrix(float* matrix, int width)
 {
     for (std::size_t i = 0; i < width*width; ++i)
     {
-        std::cout << matrix[i] << " ";
+        std::cout << matrix[i] << '\t';
         if ((i + 1) % width == 0)
         {
             std::cout << '\n';
@@ -44,33 +65,37 @@ void DisplayMatrix(float* matrix, int width)
 
 int main() 
 {
-    float matrixA[16] = {
-        1, 2, 3, 4,
-        5, 6, 7, 8,
-        9, 10, 11, 12,
-        13, 14, 15, 16
-    };
-
-    float matrixB[16] = {
-        1, 2, 3, 4,
-        5, 6, 7, 8,
-        9, 10, 11, 12,
-        13, 14, 15, 16
-    };
-
-    float resultMatrix[16];
-
-    float *matrixAd, *matrixBd, *resultMatrixd;
-
     int width = 4;
-    int matrixSize = width * width * sizeof(float);
 
-    dim3 dimBlock(16,16);
+    // Declare host matrices
+    float matrixA[width*width];
+    float matrixB[width*width];
+    float resultMatrix[width*width];
+
+    // Cuda grid configuration
+    dim3 dimBlock(4,4);
     dim3 dimGrid((width+15)/16 , (width+15)/16);
 
+    // Declare device matrices
+    float *matrixAd, *matrixBd, *resultMatrixd;
+
+    int matrixSize = width * width * sizeof(float);
+
+    // Allocate memory on device
     cudaMalloc(&matrixAd, matrixSize);
     cudaMalloc(&matrixBd, matrixSize);
     cudaMalloc(&resultMatrixd, matrixSize);
+
+    // Initialize matrices using CPU
+    InitializeMatrixCPU(matrixA, width);
+    InitializeMatrixCPU(matrixB, width);
+
+    // Initialize matrices using GPU
+    // InitializeMatrixGPU<<<dimGrid, dimBlock>>>(matrixAd, width);
+    // InitializeMatrixGPU<<<dimGrid, dimBlock>>>(matrixBd, width);
+    // cudaDeviceSynchronize();
+    // cudaMemcpy(matrixA, matrixAd, matrixSize, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(matrixB, matrixBd, matrixSize, cudaMemcpyDeviceToHost);
 
     cudaMemcpy(matrixAd, matrixA, matrixSize, cudaMemcpyHostToDevice);
     cudaMemcpy(matrixBd, matrixB, matrixSize, cudaMemcpyHostToDevice);
